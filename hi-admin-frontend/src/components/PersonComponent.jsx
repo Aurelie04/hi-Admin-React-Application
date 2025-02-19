cf5e2e5de1519e7a5e5e1c5e4e6a79047a8490a8
Aurelie04 <aurelienana25@gmail.com>
2025-02-19 14:34:14 +0200

    Validation of the form

--- hi-admin-frontend/src/components/PersonComponent.jsx
+++ hi-admin-frontend/src/components/PersonComponent.jsx
@@ -12,14 +12,23 @@ const PersonComponent = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
 
+ const [errors,setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone_number: '',
+    description: '',
+    amount: ''
+  })
+
   const navigator = useNavigate();
 
 
   const handleSubmit = (e) => {
      e.preventDefault();
- 
-
-    const person = { firstName, lastName, email, phone_number, description, amount };
+     
+     if(validateForm()){
+      const person = { firstName, lastName, email, phone_number, description, amount };
     console.log(person);
 
     createPerson(person)
@@ -32,7 +41,64 @@ const PersonComponent = () => {
           // Handle error, e.g., display an error message to the user
         });
 
+     }
+
+    
+
   };
+
+   function validateForm(){
+     let valid = true;
+
+     const errorsCopy = {... errors}
+
+     if(firstName.trim()){
+        errorsCopy.firstName = '';
+     }else{
+            errorsCopy.firstName ='first name required';
+            valid = false;
+     }
+
+     if(lastName.trim()){
+      errorsCopy.lastName = '';
+     }else{
+          errorsCopy.lastName ='last name required';
+          valid = false;
+      }
+ 
+     if(email.trim()){
+       errorsCopy.email = '';
+     }else{
+        errorsCopy.email =' email required';
+        valid = false;
+     }
+
+     if(phone_number.trim()){
+       errorsCopy.phone_number = '';
+     }else{
+      errorsCopy.firstName ='Phone number is required';
+      valid = false;
+     }
+
+     if(description.trim()){
+      errorsCopy.firstName = '';
+     }else{
+      errorsCopy.description ='Description is required';
+      valid = false;
+    }
+
+     if(amount.trim()){
+     errorsCopy.amount = '';
+     }else{
+      errorsCopy.amount ='amount is required';
+      valid = false;
+    }
+
+    setErrors(errorsCopy);
+
+    return valid;
+
+   }
 
   return (
     <Container className="mt-5 text-center"> 
@@ -51,9 +117,12 @@ const PersonComponent = () => {
                       type="text" 
                       placeholder="Enter First Name" 
                       value={firstName} 
+                      className= {`form-control ${ errors.firstName ? 'is-invalid': ''}`}
                       onChange={(e) => setFirstName(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+
+                    {errors.firstName && <div className='invalid-feedback'>{ errors.firstName}</div>}
                   </td>
                 </tr>
                 <tr>
@@ -63,9 +132,11 @@ const PersonComponent = () => {
                       type="text" 
                       placeholder="Enter Last Name" 
                       value={lastName} 
+                      className= {`form-control ${ errors.lastName ? 'is-invalid': ''}`}
                       onChange={(e) => setLastName(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+                    {errors.lastName && <div className='invalid-feedback'>{ errors.lastName}</div>}
                   </td>
                 </tr>
                 <tr>
@@ -75,9 +146,11 @@ const PersonComponent = () => {
                       type="email" 
                       placeholder="Enter Email" 
                       value={email} 
+                      className= {`form-control ${ errors.email ? 'is-invalid': ''}`}
                       onChange={(e) => setEmail(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+                    {errors.email && <div className='invalid-feedback'>{ errors.email}</div>}
                   </td>
                 </tr>
                 <tr>
@@ -87,9 +160,11 @@ const PersonComponent = () => {
                       type="text" 
                       placeholder="Enter Phone Number" 
                       value={phone_number} 
+                      className= {`form-control ${ errors.phone_number ? 'is-invalid': ''}`}
                       onChange={(e) => setPhoneNumber(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+                    {errors.phone_number && <div className='invalid-feedback'>{ errors.phone_number}</div>}
                   </td>
                 </tr>
                 <tr>
@@ -100,9 +175,11 @@ const PersonComponent = () => {
                       rows={3} 
                       placeholder="Enter Description" 
                       value={description} 
+                      className= {`form-control ${ errors.description ? 'is-invalid': ''}`}
                       onChange={(e) => setDescription(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+                    {errors.description && <div className='invalid-feedback'>{ errors.description}</div>}
                   </td>
                 </tr>
                 <tr>
@@ -112,9 +189,12 @@ const PersonComponent = () => {
                       type="text" 
                       placeholder="Enter Amount" 
                       value={amount} 
+                      className= {`form-control ${ errors.amount ? 'is-invalid': ''}`}
                       onChange={(e) => setAmount(e.target.value)} 
                       style={{ fontSize: '1.2rem', width: '100%' }} 
                     />
+                    {errors.amount && <div className='invalid-feedback'>{ errors.amount}</div>}
+                    
                   </td>
                 </tr>
               </tbody>
